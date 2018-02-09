7ede70a352622ce00bd8295443543dc47b3851e6
tako <takosama.312@gmail.com>
2018-02-09 19:15:15 +0900

    プロジェクト ファイルを追加します。

---
+++ Sketch2/Sketch2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch2.ino
+ Created:	2018/02/09 19:15:03
+ Author:	takos
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
